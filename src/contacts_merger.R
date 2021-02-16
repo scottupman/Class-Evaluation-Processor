50a8f9598aee16499ba061db36f401761859a54f
Scott <scottupman1@gmail.com>
2021-02-16 18:25:19 -0500

    Merge contacts

---
+++ src/contacts_merger.R
@@ -0,0 +1,17 @@
+winDialog(type = c("ok"),
+          "Select the Summer contact list for the academic year.\n\n
+(Example: Academic year: Summer 2019 - Fall 2020)")
+
+# Read the file
+
+winDialog(type = c("ok"),
+          "Select the Fall contact list for the academic year.\n\n
+(Example: Academic year: Summer 2019 - Fall 2020)")
+
+# Read the file
+
+winDialog(type = c("ok"),
+          "Select the Spring contact list for the academic year.\n\n
+(Example: Academic year: Summer 2019 - Fall 2020)")
+
+# Read the file
