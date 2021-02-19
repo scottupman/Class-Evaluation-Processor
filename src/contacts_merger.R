127ab50f8eebd032540d757fee2e72d06a6af11f
Scott <scottupman1@gmail.com>
2021-02-19 17:04:25 -0500

    Trying to implement a class in R

--- src/contacts_merger.R
+++ src/contacts_merger.R
@@ -1,17 +1,16 @@
-winDialog(type = c("ok"),
-          "Select the Summer contact list for the academic year.\n\n
+contact_merger <- setRefClass("contact_merger")
+
+library(tcltk)
+
+contact_merger$methods(func = function()
+{
+  winDialog(type = c("ok"),
+            "Select all the contacts files for the academic year.\n\n
 (Example: Academic year: Summer 2019 - Fall 2020)")
+  
+  file_list <- tk_choose.files(multi = TRUE)
+  contact_list <<- lapply(file_list, read.csv)
+}
+) 
 
-# Read the file
 
-winDialog(type = c("ok"),
-          "Select the Fall contact list for the academic year.\n\n
-(Example: Academic year: Summer 2019 - Fall 2020)")
-
-# Read the file
-
-winDialog(type = c("ok"),
-          "Select the Spring contact list for the academic year.\n\n
-(Example: Academic year: Summer 2019 - Fall 2020)")
-
-# Read the file
---
+++ src/untitled_file.R
@@ -0,0 +1,12 @@
+source("/Users/User/Desktop/Files/GitHub repositories/Class-Evaluation-Processor/src/contacts_merger.R")
+
+contact_merger$new()
+
+if(is.object(contact_merger))
+{
+  print("working")
+}
+
+contact_merger$func()
+
+
