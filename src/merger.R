780c1f373ce6c59f248dde4a19c93e047206cdf9
Scott <scottupman1@gmail.com>
2021-02-23 14:02:29 -0500

    I/O prompt

---
+++ src/merger.R
@@ -0,0 +1,14 @@
+library(tcltk)
+library(R6)
+
+Merger <- R6Class("Merger", public = list(
+  merge = function(data_sheet_vector) #parameter should be a vector of csv files
+  {
+    # with the list of csv files, identify each of them,
+    # Afterwards merge them together into one csv file
+  }
+))
+
+
+
+
--- src/yearly_evaluation.R
+++ src/yearly_evaluation.R
@@ -1,6 +1,26 @@
-source("/Users/User/Desktop/Files/GitHub repositories/Class-Evaluation-Processor/src/helpers.R")
+library(tcltk)
 
+source("/Users/User/Desktop/Files/GitHub repositories/Class-Evaluation-Processor/src/helpers.R")
+source("/Users/User/Desktop/Files/GitHub repositories/Class-Evaluation-Processor/src/merger.R")
+
+# Should contain the prompt and require the file inputs
 main <- function()
 {
-   
+   # Prompt user for the files
+  winDialog(type = c("ok"),
+            "Select all the contacts files for the academic year.\n\n
+(Example: Academic year: Summer 2019 - Fall 2020)")
+  
+  contacts_folder <- choose.dir();
+  evals_folder <- choose.dir();
+  
+  contacts_vector <- list.files(contacts_folder)
+  evals_vector <- list.files(evals_folder)
+  
+  for (element in contacts_vector)
+  {
+    print(element)
+  }
 }
+
+main()
