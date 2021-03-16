9d74f75ceb22c1c9f6cd181eb63097392cb5afd9
Scott <scottupman1@gmail.com>
2021-03-16 15:05:04 -0400

    Checks the lists

--- src/merger.R
+++ src/merger.R
@@ -2,7 +2,7 @@ library(tcltk)
 library(R6)
 
 Merger <- R6Class("Merger", public = list(
-  merge = function(data_sheet_vector) #parameter should be a vector of csv files
+  merge = function(data_list) #parameter should be a list of csv files
   {
     # with the list of csv files, identify each of them,
     # Afterwards merge them together into one csv file
--- src/yearly_evaluation.R
+++ src/yearly_evaluation.R
@@ -1,7 +1,9 @@
 library(tcltk)
+library(purrr)
 
 source("/Users/User/Desktop/Files/GitHub repositories/Class-Evaluation-Processor/src/helpers.R")
 source("/Users/User/Desktop/Files/GitHub repositories/Class-Evaluation-Processor/src/merger.R")
+source("/Users/User/Desktop/Files/GitHub repositories/Class-Evaluation-Processor/src/main_helpers.R")
 setwd("~/R/Evals/")
 
 # Should contain the prompt and require the file inputs
@@ -23,31 +25,16 @@ main <- function()
   evals_files <- tk_choose.files()
   
   # vectors that will store each file as a dataframe
-  contacts <- vector("list", length = 3);
-  evals <- vector("list", length = 3);
-  
-  # 
   contacts <- lapply(contacts_files, read.csv)
   evals <- lapply(evals_files, read.csv)
   
-  # Just merge the contacts and evaluation files
-  for (entry in 1:length(contacts))
-  {
-    if(names(contacts[[1]]) != names(contacts[[entry]]))
-      stop("One of the contact files have different names, please correct the data")
-  }
-  # Foreach (data frame in contacts)
-    # check if the names match
-      # if they don't match
-        # throw exception error
-      # else
-        # merge the contacts together.
+  # Checks if each data sheet has the same column names.
+  check_lists(contacts, evals)
   
-  # I feel like the data frames should have the same names for consistency
-  # But in the end, there should be one data frame for contacts and one data frame for evals
-  # Make sure that csv file formats match.
+  # Combine both the evaluation sheets and contact sheets.
   
-}
+  
 
+}
 main()
 
