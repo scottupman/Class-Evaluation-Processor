ee220b04e2d0b611dc57afade9c2064412a03a7b
Scott <scottupman1@gmail.com>
2021-03-31 14:06:50 -0400

    R6 Class

--- src/main_helpers.R
+++ src/main_helpers.R
@@ -2,16 +2,21 @@ check_lists <-function(contacts, evals)
 {
   for (entry in 1:length(contacts))
   {
+    # print(ncol(contacts[[entry]]))
+    # }
     if(names(contacts[[1]]) != names(contacts[[entry]]))
-      
+
       setdiff(names(contacts[[1]]), names(contacts[[entry]]))
       stop("One of the contact files have different column names, please make sure the data is correctly formatted")
   }
   
   for (entry in 1:length(evals))
   {
+    # print(ncol(contacts[[entry]]))
     if(names(evals[[1]]) != names(contacts[[entry]]))
       setdiff(names(contacts[[1]]), names(contacts[[entry]]))
       stop("One of the evaluation files have different column names, please make sure the data is correctly formatted")
   }
+  
 }
+
--- src/yearly_evaluation.R
+++ src/yearly_evaluation.R
@@ -1,44 +1,99 @@
 library(tcltk)
 library(dplyr)
+library(R6)
 
 source("/Users/User/Desktop/Files/GitHub repositories/Class-Evaluation-Processor/src/helpers.R")
 source("/Users/User/Desktop/Files/GitHub repositories/Class-Evaluation-Processor/src/merger.R")
 source("/Users/User/Desktop/Files/GitHub repositories/Class-Evaluation-Processor/src/main_helpers.R")
 setwd("~/R/Evals/")
 
-# Should contain the prompt and require the file inputs
-main <- function()
-{
-   # Prompt user for the files
-  winDialog(type = c("ok"),
-            "Select the contacts files for the academic year.\n\n
+YearEval <- R6Class("YearEval", public = list(
+  
+  yearly_evals = data.frame(matrix(ncol = 15)),
+  
+  initialize = function()
+  {
+    colnames(yearly_evals) <- c("Course", "Question", "Poor", "Fair", "Good", "Very Good", "Excellent",
+                                "Total Responses", "Class size", "Semester", "Year", "Course Director",
+                                "Dep", "Rating", "Weighted Rating")
+  },
+  
+  create_merged_contacts = function() 
+  {
+    winDialog(type = c("ok"),
+              "Select the contacts files for the academic year.\n\n
 (Example: Academic year: Summer 2019 - Fall 2020)")
-  
-  # vector that stores each contact file
-  contacts_files <- tk_choose.files()
-  
-  winDialog(type = c("ok"),
-            "Select the evaluations files for the academic year.\n\n
+    
+    # vector that stores each contact file
+    contacts_files <- tk_choose.files()
+    
+    # vectors that will store each file as a dataframe
+    contacts <- lapply(contacts_files, function(i)
+    {read.csv(i, stringsAsFactors = F, encoding = "UTF-8")})
+    
+    # Combined data frames of the contacts and evaluation files. 
+    merged_contacts <- bind_rows(contacts, .id = NULL)
+    return(merged_contacts)
+    
+    
+  },
+  create_merged_evals = function()
+  {
+    winDialog(type = c("ok"),
+              "Select the evaluations files for the academic year.\n\n
 (Example: Academic year: Summer 2019 - Fall 2020)")
-  
-  # vector that stores each eval file
-  evals_files <- tk_choose.files()
-  
-  # vectors that will store each file as a dataframe
-  contacts <<- lapply(contacts_files, function(i)
-  {read.csv(i, stringsAsFactors = F, fileEncoding = "UTF-8-BOM")})
+    
+    # vector that stores each eval file
+    evals_files <- tk_choose.files()
+    
+    evals <- lapply(evals_files, function(i)
+    {read.csv(i, stringsAsFactors = F, encoding = "UTF-8")})
+    
+    merged_evals <- bind_rows(evals, .id = NULL)
+    return(merged_evals)
+  }
+))
 
-  
-  evals <<- lapply(evals_files, function(i)
-  {read.csv(i, stringsAsFactors = F, fileEncoding = "UTF-8-BOM")})
-  
-  # Checks if each data sheet has the same column names.
-  check_lists(contacts, evals) 
-  df1 <<- bind_rows(contacts, .id = NULL)
-  df2 <<- bind_rows(evals, .id = NULL)
-  
-  # Combine both the evaluation sheets and contact sheets.
-  
-}
-main()
+# yearly_evals <- data.frame(matrix(ncol = 15))
+# colnames(yearly_evals) <- c("Course", "Question", "Poor", "Fair", "Good", "Very Good", "Excellent",
+#                             "Total Responses", "Class size", "Semester", "Year", "Course Director",
+#                             "Dep", "Rating", "Weighted Rating")
+# 
+# # Should contain the prompt and require the file inputs
+# main <- function()
+# {
+#    # Prompt user for the files
+#   winDialog(type = c("ok"),
+#             "Select the contacts files for the academic year.\n\n
+# (Example: Academic year: Summer 2019 - Fall 2020)")
+#   
+#   # vector that stores each contact file
+#   contacts_files <- tk_choose.files()
+#   
+#   
+#   winDialog(type = c("ok"),
+#             "Select the evaluations files for the academic year.\n\n
+# (Example: Academic year: Summer 2019 - Fall 2020)")
+#   
+#   # vector that stores each eval file
+#   evals_files <- tk_choose.files()
+#   
+#   # vectors that will store each file as a dataframe
+#   contacts <- lapply(contacts_files, function(i)
+#   {read.csv(i, stringsAsFactors = F, encoding = "UTF-8")})
+# 
+#   
+#   evals <- lapply(evals_files, function(i)
+#   {read.csv(i, stringsAsFactors = F, encoding = "UTF-8")})
+#   
+#   # Checks if each data sheet has the same column names.
+#   # check_lists(contacts, evals)
+#   
+#   # Combined data frames of the contacts and evaluation files. 
+#   merged_contacts <<- bind_rows(contacts, .id = NULL)
+#   merged_evals <<- bind_rows(evals, .id = NULL)
+#   
+# }
+# 
+# main()
 
